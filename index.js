9970ce628acc8f2f0cce994dffa1f1a6f19e0c5d
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-13 19:35:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let returnString = "";
+    let inputArray = string.split(" ");
+    for (let i = 0; i < inputArray.length; i++){
+      if (i === 0){
+        returnString += `${this.capitalize(inputArray[i])}`
+      } else {
+        switch (inputArray[i]) {case "the":, case "a":, case "an":, case "but":, case "of":, case "and":, case "for":, case "at":, case "by":, case "from":
+            returnString += `${inputArray[i]}`;
+            break;
+          default:
+            returnString += `${this.capitalize(inputArray[i])}`;
+        }
+      }
+      if (i !== inputArray.length - 1){
+        returnString += " ";
+      }
+    }
+    returnString.slice(-1);
+    return returnString;
+  }
+
 }
