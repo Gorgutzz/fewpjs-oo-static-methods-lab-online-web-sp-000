2332d9500c8f0e0ec3e18801b0ae4be047dc4cd3
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-13 19:43:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9- ']+/g, '');
   }
 
-  static titleize(str) {
-    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+  static titleize(string) {
+    let excluded = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let result = [];
-    let arrayOfWords = str.split(" ")
-    for (let n = 0; n < arrayOfWords.length; n++) {
+    let array = str.split(" ")
+    for (let n = 0; n < array.length; n++) {
       if (n == 0) {
-        result.push(this.capitalize(arrayOfWords[n]))
+        result.push(this.capitalize(array[n]))
       }
       else {
-        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
-          result.push( arrayOfWords[ n ] )
+        if ( excluded.includes( array[ n ] ) ) {
+          result.push( array[ n ] )
         } else {
-          result.push( this.capitalize( arrayOfWords[ n ] ) )
+          result.push( this.capitalize( array[ n ] ) )
         }
       }
 
